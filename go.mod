bd395ce4bc8cf0013c486d9b5355f5262d01cd9f
LeafHacker <leafhacker@yahoo.com>
2019-09-05 19:22:37 +0100

    Use the echo web framework
    Let's not re-implement the web ok, this framework should be lightweight
    enough to not get in the way.

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/ImpactDevelopment/ImpactServer
 
 go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
+	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"os"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "max-age=300")
-		fs.ServeHTTP(w, r)
-	})
+	// Echo is cool https://echo.labstack.com
+	e := echo.New()
+
+	// Enforce URL style
+	// FIXME forwarding to https breaks localhost testing cos I haven't setup a TLS cert
+	// See https://echo.labstack.com/cookbook/auto-tls
+	//e.Pre(middleware.HTTPSNonWWWRedirect())
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// Fall back to static files
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:   "static", // Root directory from where the static content is served.
+		Browse: false,    // Don't enable directory browsing.
+		HTML5:  false,    // Don't forward everything to root (would allow client-side routing)
+	}))
+
+	// Compress responses
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+
+	// Log all the things TODO formatting https://echo.labstack.com/middleware/logger
+	e.Use(middleware.Logger())
+
+	// Don't crash
+	e.Use(middleware.Recover())
+
+	// Get the specified port
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	// Start the server
+	e.Logger.Fatal(e.Start(":" + port))
 }
