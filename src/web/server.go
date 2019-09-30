c27f2d308c6ab402b0a8643b00a507195ad22528
LeafHacker <leafhacker@yahoo.com>
2019-09-30 20:35:13 +0100

    We don't need an entire new server for Static
    Use a Group to do the same thing. When labstack/echo#1407 is merged we
    won't need any work arounds at all.

--- src/web/server.go
+++ src/web/server.go
@@ -1,9 +1,8 @@
 package web
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/middleware"
+	"net/http"
 
 	mid "github.com/ImpactDevelopment/ImpactServer/src/middleware"
 	"github.com/labstack/echo"
@@ -19,13 +18,8 @@ func Server() (e *echo.Echo) {
 	e.GET("/ImpactInstaller.jar", installerForJar, mid.Cache(0))
 	e.GET("/ImpactInstaller.exe", installerForExe, mid.Cache(0))
 
-	staticEcho := echo.New()
-	staticEcho.Use(mid.Cache(86400))
-	staticEcho.Static("/", "static")
-	e.Any("/*", func(c echo.Context) error {
-		staticEcho.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
+	// Use e.Group since e.Static doesn't allow setting middleware, see labstack/echo#1407
+	_ = e.Group("/", middleware.Static("static"), mid.Cache(86400))
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
