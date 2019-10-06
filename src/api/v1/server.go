59667a71e1cbab7d8d60cbfc5d0b836d815873bc
LeafHacker <leafhacker@yahoo.com>
2019-10-06 19:41:58 +0100

    Longer cache xD

--- src/api/v1/server.go
+++ src/api/v1/server.go
@@ -9,6 +9,6 @@ import (
 func API(api *echo.Group) {
 	// TODO API Doc
 
-	api.GET("/motd", getMotd, middleware.Cache(60*10))
-	api.GET("/minecraft/user/info", getUserInfo, middleware.Cache(60*10))
+	api.GET("/motd", getMotd, middleware.Cache(60*30))
+	api.GET("/minecraft/user/info", getUserInfo, middleware.Cache(60*30))
 }
