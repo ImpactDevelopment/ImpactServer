06414564f753951fa99b6a73bddece5749ef6b32
LeafHacker <leafhacker@yahoo.com>
2019-10-06 17:59:03 +0100

    Remove commented API proposals

--- src/api/v1/server.go
+++ src/api/v1/server.go
@@ -7,7 +7,5 @@ import (
 // TODO API Doc
 func API(api *echo.Group) {
 	api.GET("/motd", getMotd)
-	//api.GET("/version/:project/:version", getVersionInfo) // Get info about mc or impact versions, e.g. which versions of the former the latter supports.
 	api.GET("/minecraft/user/info", getUserInfo)
-	//api.GET("/minecraft/user/legacy/:type", legacyMCUserInfo) //DEPRECATED: Get a list of UUIDs for the given type
 }
