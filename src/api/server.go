7666c7eef1345a762a4da8b521af561d7c55b72d
LeafHacker <leafhacker@yahoo.com>
2019-12-29 05:29:54 +0000

    Include CORS header in API responses

--- src/api/server.go
+++ src/api/server.go
@@ -3,11 +3,15 @@ package api
 import (
 	"github.com/ImpactDevelopment/ImpactServer/src/api/v1"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // Server returns an echo server that handles api requests for each version
 func Server() (e *echo.Echo) {
 	e = echo.New()
+
+	// Allow browser clients to use the API
+	e.Use(middleware.CORS())
 
 	v1.API(e.Group("/v1"))
 
