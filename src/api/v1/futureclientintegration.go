493d8570f33aa56bfb168d08fbd03885a468a0f9
LeafHacker <leafhacker@yahoo.com>
2020-01-25 18:48:23 +0000

    futureIntegration strings.Builder

--- src/api/v1/futureclientintegration.go
+++ src/api/v1/futureclientintegration.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/ImpactDevelopment/ImpactServer/src/database"
 	"github.com/labstack/echo/v4"
@@ -12,23 +13,26 @@ func futureIntegration(c echo.Context) error {
 	if c.QueryParam("auth")+"0" != os.Getenv("API_AUTH_SECRET") {
 		return c.JSON(http.StatusForbidden, "auth wrong im sowwy")
 	}
+
 	rows, err := database.DB.Query("SELECT mc_uuid FROM users WHERE spawnmason")
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
-	ret := ""
+
+	var b strings.Builder
 	for rows.Next() {
 		var uuidStr string
 		err = rows.Scan(&uuidStr)
 		if err != nil {
 			return err
 		}
-		ret += uuidStr + "\n"
+		b.WriteString(uuidStr + "\n")
 	}
 	err = rows.Err()
 	if err != nil {
 		return err
 	}
-	return c.String(http.StatusOK, ret)
+
+	return c.String(http.StatusOK, b.String())
 }
