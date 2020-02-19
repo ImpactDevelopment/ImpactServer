86cb188ecadfb77503a6265bbe8c12de9a50fe41
LeafHacker <leafhacker@yahoo.com>
2020-02-19 19:41:15 +0000

    btfo special case for 4.8.1 premium
    Just upgrade to 4.8.3, there is no reason to need premium features on 4.8.1

--- src/api/v1/server.go
+++ src/api/v1/server.go
@@ -19,7 +19,6 @@ func API(api *echo.Group) {
 	api.GET("/minecraft/user/info", getUserInfo, middleware.CacheUntilPurge())
 	api.GET("/minecraft/user/:role/list", getRoleMembers, middleware.CacheUntilPurge())
 	api.GET("/dbtest", dbTest, middleware.NoCache())
-	api.GET("/minecraft/login", mojangLoginLegacy, middleware.NoCache()) // TODO remove; this is only used by 4.8.1's deprecated premium login
 	api.Match([]string{http.MethodGet, http.MethodPost}, "/login/minecraft", jwt.MinecraftLoginHandler, middleware.NoCache())
 	api.Match([]string{http.MethodGet, http.MethodPost}, "/login/discord", jwt.DiscordLoginHandler, middleware.NoCache())
 	api.Match([]string{http.MethodGet, http.MethodPost}, "/paypal/afterpayment", afterDonation, middleware.NoCache())
