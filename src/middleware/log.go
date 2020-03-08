91b3ce87417261ed50bc467d1af53fe1832d5863
LeafHacker <leafhacker@yahoo.com>
2020-03-08 15:31:38 +0000

    Include host

--- src/middleware/log.go
+++ src/middleware/log.go
@@ -3,5 +3,5 @@ package middleware
 import "github.com/labstack/echo/v4/middleware"
 
 var Log = middleware.LoggerWithConfig(middleware.LoggerConfig{
-	Format: "${status} ${method} ${uri} latency=${latency_human} error=${error}\n",
+	Format: "${status} ${method} ${host}${uri} latency=${latency_human} error=${error}\n",
 })
