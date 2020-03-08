c268bc0c8fe441fcd43821143b9b33d43b21c6b4
Leijurv <leijurv@gmail.com>
2020-03-08 15:24:33 -0700

    epic log format

--- src/middleware/log.go
+++ src/middleware/log.go
@@ -3,5 +3,5 @@ package middleware
 import "github.com/labstack/echo/v4/middleware"
 
 var Log = middleware.LoggerWithConfig(middleware.LoggerConfig{
-	Format: "${status} ${method} ${host}${uri} latency=${latency_human} error=${error}\n",
+	Format: "#${header:X-Request-ID} ${status} ${method} ${host}${uri} latency=${latency} [${latency_human}] error=${error}\n",
 })
