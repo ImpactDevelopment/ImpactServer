ImpactDevelopment/ImpactServer

commit 2d8a2d0f9edbe075f743581ff6b166185c9f0cc0

Lookup token before using it

src/api/v1/register.go
@@ -6,9 +6,11 @@ import (
 	"github.com/ImpactDevelopment/ImpactServer/src/middleware"
 	"github.com/ImpactDevelopment/ImpactServer/src/minecraft"
 	"github.com/ImpactDevelopment/ImpactServer/src/users"
+	"github.com/lib/pq"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ImpactDevelopment/ImpactServer/src/database"
 	"github.com/ImpactDevelopment/ImpactServer/src/discord"
@@ -17,38 +19,65 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type registrationCheck struct {
-	Token string `json:"token" form:"token" query:"token"`
-}
-
-type registration struct {
-	Token        string `json:"token" form:"token" query:"token"`
-	DiscordToken string `json:"discord" form:"discord" query:"discord"`
-	Minecraft    string `json:"minecraft" form:"minecraft" query:"minecraft"`
-	Email        string `json:"email" form:"email" query:"email"`
-	Password     string `json:"password" form:"password" query:"password"`
-}
-
 func checkToken(c echo.Context) error {
-	body := &registrationCheck{}
-	err := c.Bind(body)
+	var body struct {
+		Token string `json:"token" form:"token" query:"token"`
+	}
+
+	err := c.Bind(&body)
 	if err != nil {
 		return err
 	}
 	if body.Token == "" {
 		return echo.NewHTTPError(http.StatusBadRequest, "token missing")
 	}
-	var createdAt int64
-	err = database.DB.QueryRow("SELECT created_at FROM pending_donations WHERE token = $1 AND NOT used", body.Token).Scan(&createdAt)
+	token, err := getToken(body.Token)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "invalid token")
+		return err
 	}
-	return c.String(200, "true")
+
+	var createdAt int64
+	var roles pq.StringArray
+	err = database.DB.QueryRow(`
+		SELECT
+			created_at,
+			STRING_TO_ARRAY(
+				CONCAT_WS(',',
+					CASE WHEN premium THEN 'premium' END,
+					CASE WHEN pepsi THEN 'pepsi' END,
+					CASE WHEN spawnmason THEN 'spawnmason' END,
+					CASE WHEN staff THEN 'staff' END
+				),
+				','
+			) AS roles
+		FROM pending_donations
+		WHERE token = $1 AND NOT used`, token).Scan(&createdAt, &roles)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return echo.NewHTTPError(http.StatusBadRequest, "invalid token").SetInternal(err)
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error()).SetInternal(err)
+	}
+
+	return c.JSON(http.StatusOK, struct {
+		CreatedAt string   `json:"created_at"`
+		Roles     []string `json:"roles"`
+	}{
+		CreatedAt: time.Unix(createdAt, 0).UTC().Format(time.RFC3339),
+		Roles:     roles,
+	})
 }
 
 func registerWithToken(c echo.Context) error {
+	var body struct {
+		Token        string `json:"token" form:"token" query:"token"`
+		DiscordToken string `json:"discord" form:"discord" query:"discord"`
+		Minecraft    string `json:"minecraft" form:"minecraft" query:"minecraft"`
+		Email        string `json:"email" form:"email" query:"email"`
+		Password     string `json:"password" form:"password" query:"password"`
+	}
+
 	authedUser := middleware.GetUser(c)
-	body := &registration{}
 	err := c.Bind(body)
 	if err != nil {
 		return err
@@ -59,9 +88,8 @@ func registerWithToken(c echo.Context) error {
 	}
 
 	// Verify the registration token
-	// TODO get roles from token
-	body.Token = strings.TrimSpace(body.Token)
 	var (
+		token      *uuid.UUID
 		createdAt  int64
 		amount     int64
 		used       bool
@@ -73,7 +101,11 @@ func registerWithToken(c echo.Context) error {
 	)
 	// token can be omitted if logged in
 	if body.Token != "" {
-		err = database.DB.QueryRow("SELECT created_at, amount, used, log_msg_id, premium, pepsi, spawnmason, staff FROM pending_donations WHERE token = $1", body.Token).Scan(&createdAt, &amount, &used, &logID, &premium, &pepsi, &spawnmason, &staff)
+		token, err = getToken(body.Token)
+		if err != nil {
+			return err
+		}
+		err = database.DB.QueryRow("SELECT created_at, amount, used, log_msg_id, premium, pepsi, spawnmason, staff FROM pending_donations WHERE token = $1", token).Scan(&createdAt, &amount, &used, &logID, &premium, &pepsi, &spawnmason, &staff)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, "invalid token")
 		}
@@ -177,8 +209,8 @@ func registerWithToken(c echo.Context) error {
 	}
 
 	// TODO should we just DELETE the token?
-	if body.Token != "" {
-		_, err = tx.Exec("UPDATE pending_donations SET used = true, used_by = $1 WHERE token = $2", userID, body.Token)
+	if token != nil {
+		_, err = tx.Exec("UPDATE pending_donations SET used = true, used_by = $1 WHERE token = $2", userID, token)
 		if err != nil {
 			log.Print(err.Error())
 			return err
@@ -235,9 +267,16 @@ func registerWithToken(c echo.Context) error {
 	if user == nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "successfully registered, but can't find user")
 	}
-	token := jwt.CreateUserJWT(user)
 
-	return c.String(http.StatusOK, token)
+	return c.String(http.StatusOK, jwt.CreateUserJWT(user))
+}
+
+func getToken(token string) (*uuid.UUID, error) {
+	tokenID, err := uuid.Parse(strings.TrimSpace(token))
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid token format").SetInternal(err)
+	}
+	return &tokenID, nil
 }
 
 func verifyEmail(email string) (string, error) {

static/js/api.js
@@ -97,6 +97,20 @@
                 })
             })
         },
+        getTokenInfo: function(token) {
+            return new Promise(function (resolve, reject) {
+                $.get({
+                    url: baseUrl + "/checktoken?token=" + encodeURIComponent(token),
+                    dataType: "json",
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        reject(errorThrown + ": " + messageFromjqXHR(jqXHR))
+                    },
+                    success: function (data, status) {
+                        resolve(data)
+                    }
+                })
+            })
+        },
         confirmPayment: function(orderID) {
             return new Promise(function (resolve, reject) {
                 $.post({

static/register.html
@@ -99,6 +99,24 @@
             return errors.length < 1 && !unlinked
         }
 
+        function oxfordJoin(arr){
+            let l = arr.length;
+            if (l < 1) return "";
+            if (l === 1) return arr[0];
+            if (l === 2) return arr.join(" & ");
+            arr = arr.slice(); // Don't mutate the original
+            arr[l-1] = "& "+arr[l-1];
+            return arr.join(", ");
+        }
+
+        function redirect(url) {
+            try {
+                window.location.replace(url);
+            } catch (e) {
+                window.location = url;
+            }
+        }
+
         function gtag() {
             if (!window.dataLayer) {
                 window.dataLayer = []
@@ -311,7 +329,7 @@
             <label for="token" data-error="Invalid token">
                 Registration token
             </label>
-            <span class="helper-text">
+            <span class="helper-text token-info">
                 Impact Accounts require a token generated when you donated
             </span>
         </div>
@@ -365,10 +383,15 @@
         if (token) {
             // Hide the label and set the input value if a token was passed in
             // We don't care if it is valid at this point
-            $("#token_field").addClass("hidden")
+            $("#token_field")
+                .addClass("hidden")
+                .after('<div class="input-field col s12"><p>Token: <code>'+token+'</code></p><p class="token-info"></p></div>')
+
             var tokenField = document.getElementById("token")
             tokenField.value = token
             tokenField.type = "hidden"
+
+            setTokenInfo(token)
         }
 
         if (api.isLoggedIn()) {
@@ -409,6 +432,10 @@
                 // TODO create some method for existing users to use a token to add roles to their account
                 $("#token_field").addClass("hidden")
 
+                if (token) {
+                    setTokenInfo(token)
+                }
+
                 $(".unhide").removeClass("unhide hidden")
             })
             .catch(function (error) {
@@ -417,6 +444,37 @@
             })
         } else {
             $(".unhide").removeClass("unhide hidden")
+        }
+
+        function setTokenInfo(token) {
+            token = token.trim()
+            // lookup token info _if_ token is a valid uuid
+            if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(token)) {
+                api.getTokenInfo(token)
+                    .then(function (info) {
+                        var text = "";
+                        switch (info.roles.length) {
+                            case 0: text += "This token is valid but grants no roles! "; break
+                            case 1: {
+                                var role = info.roles[0]
+                                text += "This token will create a <b>" + role + "</b> account. "
+                            } break
+                            default: text += "This token will create an account with <b>" + oxfordJoin(info.roles) + "</b> status. "
+                        }
+                        if (api.isLoggedIn() && api.user && api.user.roles.length > 0) {
+                            text += "You already have <b>" + oxfordJoin(api.user.roles) + "</b> status on your account. "
+                        }
+                        $(".token-info").html(text)
+                    })
+                    .catch(function (error) {
+                        if (error === "Bad Request: invalid token") {
+                            error = "Invalid token"
+                        }
+                        $(".token-info").text(error)
+                    })
+            } else {
+                $(".token-info").text("Invalid token format")
+            }
         }
 
         // Called by the oauth popup window
@@ -561,6 +619,11 @@
                 })
         }
         $("#minecraft_name").keyup(updateMinecraftInfo).blur(updateMinecraftInfo)
+
+        function updateToken(event) {
+            setTokenInfo(this.value)
+        }
+        $("#token").keyup(updateToken).blur(updateToken)
 
         $("form").on("submit", function(event) {
             event.preventDefault()

static/upgrade.html
@@ -176,12 +176,12 @@
         <form novalidate id="upgrade_form" class="row">
             <!-- Materialize doesn't support text input nested within label, so we need a for-id relationship -->
             <div class="input-field col s10" id="token_field">
-                <input type="text" name="token" id="token" class="validate" required pattern="[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}">
+                <input type="text" name="token" id="token" class="validate" required pattern="\s*[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\s*">
                 <label for="token" data-error="Invalid token">
                     Token
                 </label>
-                <span class="helper-text">
-                    Impact Accounts require a token generated when you donated
+                <span class="helper-text token-info">
+                    Please enter a token
                 </span>
             </div>
             <div class="input-field col s2">
@@ -396,22 +396,64 @@
             // validate token
             if (!form.token.value) {
                 err.push("Token is required")
+            } else if (!/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(form.token.value.trim())) {
+                err.push("Token is in invalid format")
             }
 
             return err
         }
 
         function onEditUpgradeForm (event) {
-            // Don't use jquery here; the native DOM API let's us do form[name] and form.elements
-            var form = document.getElementById("upgrade_form");
+            var token = document.getElementById("upgrade_form").token.value.trim()
 
-            // TODO lookup roles token would grant? and/or check it is valid?
+            // lookup token info _if_ token is a valid uuid
+            if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(token)) {
+                api.getTokenInfo(token)
+                .then(function (info) {
+                    if (!api.user) {
+                        // This can be called early before api.user is setup
+                        return
+                    }
+
+                    var text = "";
+                    if (info.roles.length < 1) {
+                        text += "This token is valid but grants no roles!"
+                    } else {
+                        var duplicates = []
+                        var added = []
+                        info.roles.forEach(function (role) {
+                            if (api.user.roles.includes(role)) {
+                                duplicates.push(role)
+                            } else {
+                                added.push(role)
+                            }
+                        })
+                        if (added.length > 0) {
+                            text += "This token will add <b>" + oxfordJoin(added) + "</b> status to your Impact Account. "
+                        }
+                        if (duplicates.length > 0) {
+                            text += "<strong>WARNING:</strong> This token contains <b>" + oxfordJoin(duplicates) + "</b>, which you already have! "
+                        }
+                    }
+                    $(".token-info").html(text)
+                    $("#upgrade_form button[type=submit]").attr("disabled", false)
+                })
+                .catch(function (error) {
+                    if (error === "Bad Request: invalid token") {
+                        error = "Invalid token"
+                    }
+                    $("#info_msg").text(error)
+                    $("#upgrade_form button[type=submit]").attr("disabled", true)
+                })
+            } else {
+                $(".token-info").text("Please enter a token")
+            }
 
             var errors = validateUpgradeForm()
             $("#info_msg").html(errors.join("<br>"))
 
-            // Disable submit if invalid
-            $("#upgrade_form button[type=submit]").attr("disabled", errors.length > 0)
+            // Disable submit, token lookup will enable it if valid
+            $("#upgrade_form button[type=submit]").attr("disabled", true)
         }
         $("#upgrade_form")
             .submit(function (event) {
@@ -420,20 +462,8 @@
                 // Don't submit if the submit button is disabled (prevent submit-on-enter if no change)
                 if ($("#upgrade_form button[type=submit]").attr("disabled")) return;
 
-                // jquery's serialize() method won't include unchecked checkboxes, so we should handle this ourselves
-                var form = document.getElementById("upgrade_form")
-                var data = {
-                    token: form.token.value
-                }
-                $.each(document.getElementById("upgrade_form").elements, function (i, elm) {
-                    if (api.user.hasOwnProperty(elm.name)) {
-                        data[elm.name] = castToUserType(elm);
-                    } else if (["password"].includes(elm.name)) {
-                        data[elm.name] = elm.value
-                    }
-                })
-
-                api.register(data)
+                var token = document.getElementById("upgrade_form").token.value
+                api.register({token: token.trim()})
                     .then(function (response) {
                         window.location = "/account"
                     })
@@ -450,12 +480,11 @@
 
             $("#token_field")
                 .addClass("hidden")
-                .after('<p class="input-field col s12">Token: <code>'+token+'</code></p>')
+                .after('<div class="input-field col s12"><p>Token: <code>'+token+'</code></p><p class="token-info"></p></div>')
 
             var tokenField = document.getElementById("token")
             tokenField.value = token
             tokenField.type = "hidden"
-            onEditUpgradeForm()
         }
 
         function getRegisterURL() {
@@ -505,6 +534,9 @@
                     } else {
                         setMinecraftInfo()
                     }
+
+                    // Initialise form
+                    onEditUpgradeForm()
 
                     $("#upgrade").removeClass("invisible")
                 })

commit 8165fc1e4c364e2eacc0d932ac89f31cf4741b25

Fix register endpoint
2d8a2d0f9edbe075f743581ff6b166185c9f0cc0 introduced a bug where c.Bind(body) was no longer being passed a pointer.

src/api/v1/register.go
@@ -78,7 +78,7 @@ func registerWithToken(c echo.Context) error {
 	}
 
 	authedUser := middleware.GetUser(c)
-	err := c.Bind(body)
+	err := c.Bind(&body)
 	if err != nil {
 		return err
 	}