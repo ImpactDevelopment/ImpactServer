d9683568ba89a9ec2567a45483db90b6a463f717
Joe Rezendes <odd@hey.com>
2020-08-10 14:13:20 -0700

    Update user_info.go

--- src/users/user_info.go
+++ src/users/user_info.go
@@ -77,13 +77,13 @@ var specialCases = map[uuid.UUID]roleTemplate{ // TODO this should basically jus
 	},
 	// HermeticLock
 	uuid.MustParse("e97ff4c0-48bf-4c98-be34-248fdde2ffd3"): {
-	        info: &UserInfo{
+		info: &UserInfo{
 			TextColor: "RED",
 			Icon:      "https://i.imgur.com/aKt1g4H.jpg",
 			Cape:      "https://i.imgur.com/bvhC1Xk.png",
 		},
 		edition: &Edition{
-		        Icon: "https://i.imgur.com/aKt1g4H.jpg",
+			Icon: "https://i.imgur.com/aKt1g4H.jpg",
 		},
 	},
 }
