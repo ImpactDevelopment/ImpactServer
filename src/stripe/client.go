6ab203ab3d6b23e21ae911fdf59c281e70a8f618
LeafHacker <leafhacker@yahoo.com>
2020-12-27 00:08:02 +0000

    Initial work on getting a token after payment
    Mostly functional, haven't edited the DB scheme yet to test the insert query

--- src/stripe/client.go
+++ src/stripe/client.go
@@ -11,27 +11,48 @@ func init() {
 }
 
 type Payment struct {
-	ClientSecret string `json:"client_secret" form:"client_secret" query:"client_secret"`
-	Amount       int64  `json:"amount" form:"amount" query:"amount"`
-	Currency     string `json:"currency" form:"currency" query:"currency"`
+	PaymentIntent *stripe.PaymentIntent `json:"-" form:"-" query:"-" xml:"-"`
+	ClientSecret  string                `json:"client_secret" form:"client_secret" query:"client_secret"`
+	Amount        int64                 `json:"amount" form:"amount" query:"amount"`
+	Currency      string                `json:"currency" form:"currency" query:"currency"`
+	Email         string                `json:"email" form:"email" query:"email"`
 	//USDAmount int64 // TODO fetch the USD equivalent to allow granting premium on other currencies
 }
 
-func CreatePayment(amount int64, currency string, description string) (*Payment, error) {
+func CreatePayment(amount int64, currency string, description string, email string) (*Payment, error) {
 	params := &stripe.PaymentIntentParams{
 		Amount:      stripe.Int64(amount),
 		Currency:    stripe.String(currency),
 		Description: stripe.String(description),
 	}
-	intent, err := paymentintent.New(params)
+	if email != "" {
+		params.ReceiptEmail = stripe.String(email)
+	}
+	payment, err := paymentintent.New(params)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Payment{
-		ClientSecret: intent.ClientSecret,
-		Amount:       intent.Amount,
-		Currency:     intent.Currency,
+		PaymentIntent: payment,
+		ClientSecret:  payment.ClientSecret,
+		Amount:        payment.Amount,
+		Currency:      payment.Currency,
+		Email:         payment.ReceiptEmail,
 	}, nil
+}
 
+func GetPayment(id string) (*Payment, error) {
+	payment, err := paymentintent.Get(id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Payment{
+		PaymentIntent: payment,
+		ClientSecret:  payment.ClientSecret,
+		Amount:        payment.Amount,
+		Currency:      payment.Currency,
+		Email:         payment.ReceiptEmail,
+	}, nil
 }
